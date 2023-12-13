f18bee9e3de0bfeed5b2398f3ed4e30565810c68
hannah <hannahellis4242@gmail.com>
2023-12-13 15:52:16 +0000

    and compiling again

--- app/controllers/SequenceController.scala
+++ app/controllers/SequenceController.scala
@@ -1,9 +1,11 @@
 package controllers
 
-import play.api.libs.json.{JsValue, Json}
-import play.api.mvc._
-import solver.Solver.solve
+import play.api._
+import libs.json.Json
+import mvc._
+import service.Service.{readSolution, solve}
 
+import java.util.UUID
 import javax.inject.Inject
 
 class SequenceController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
@@ -16,6 +18,12 @@ class SequenceController @Inject()(val controllerComponents: ControllerComponent
       .map(x=>solve(x))
       .map(x=>Ok(Json.toJson(x)))
       .getOrElse(BadRequest(Json.toJson("")))
+  }
+
+  def get(key: UUID): Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
+    readSolution(key)
+      .map(x=>Ok(Json.toJson(x)))
+      .getOrElse(NotFound(""))
   }
 }
 
