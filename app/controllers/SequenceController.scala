28043831660fd28ccd3068520962016dee94247b
hannah <hannahellis4242@gmail.com>
2023-12-13 14:40:58 +0000

    Fleshing out endpoints. Service still needs to connect to database.

---
+++ app/controllers/SequenceController.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api._
+import libs.json.Json
+import mvc._
+import service.Service.{readSolution, solve}
+
+import java.util.UUID
+import javax.inject.Inject
+
+class SequenceController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def post(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    request
+      .body
+      .asJson
+      .map(_.validate[List[Int]])
+      .flatMap(_.asOpt)
+      .map(x=>solve(x))
+      .map(x=>Ok(Json.toJson(x)))
+      .getOrElse(BadRequest(Json.toJson("")))
+  }
+
+  def get(key: UUID): Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
+    readSolution(key)
+      .map(x=>Ok(Json.toJson(x)))
+      .getOrElse(NotFound(""))
+  }
+}
