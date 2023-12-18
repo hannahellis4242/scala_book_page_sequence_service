d774325291a021e8381158abae41e9337cb4b3c7
hannah <hannahellis4242@gmail.com>
2023-12-18 11:57:40 +0000

    Fixing a mistake

--- app/controllers/SequenceController.scala
+++ app/controllers/SequenceController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.libs.json.Json
 import play.api.mvc._
-import service.Service
+import service.Service.{solveAndSave,readSolution}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -15,44 +15,15 @@ class SequenceController @Inject()(val controllerComponents: ControllerComponent
       .asJson
       .map(_.validate[List[Int]])
       .flatMap(_.asOpt)
-      .map(x=>Service.solveAndSave(x).map((s)=>Ok(Json.toJson(s))))
+      .map(x=>solveAndSave(x).map((s)=>Ok(Json.toJson(s))))
       .getOrElse(Future{BadRequest("")})
 }
 
   def get(key: String): Action[AnyContent] = Action.async {
-    Service.readSolution(key)
+    readSolution(key)
       .map(_
         .map(value=>Ok(Json.toJson(value)))
         .getOrElse(NotFound(""))
       )
-  }
-}
-
-package controllers
-
-import play.api._
-import libs.json.Json
-import mvc._
-import service.Service.{readSolution, solve}
-
-import java.util.UUID
-import javax.inject.Inject
-
-class SequenceController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def post(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    request
-      .body
-      .asJson
-      .map(_.validate[List[Int]])
-      .flatMap(_.asOpt)
-      .map(x=>solve(x))
-      .map(x=>Ok(Json.toJson(x)))
-      .getOrElse(BadRequest(Json.toJson("")))
-  }
-
-  def get(key: UUID): Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
-    readSolution(key)
-      .map(x=>Ok(Json.toJson(x)))
-      .getOrElse(NotFound(""))
   }
 }
