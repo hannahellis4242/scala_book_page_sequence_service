750b1eda4f912d4e21707f5e078f38274a7a6c88
Hannah Ellis <hannahellis4242@gmail.com>
2023-12-19 15:41:35 +0000

    Getting endpoints working and storing solutions in mongoDB
    Getting endpoints working and storing solutions in mongoDB

--- app/controllers/SequenceController.scala
+++ app/controllers/SequenceController.scala
@@ -1,28 +1,29 @@
 package controllers
 
-import play.api._
-import libs.json.Json
-import mvc._
-import service.Service.{readSolution, solve}
+import play.api.libs.json.Json
+import play.api.mvc._
+import service.Service.{solveAndSave,readSolution}
 
-import java.util.UUID
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
-class SequenceController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def post(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+class SequenceController @Inject()(val controllerComponents: ControllerComponents,
+                                   implicit val myExecutionContext: ExecutionContext) extends BaseController {
+  def post(): Action[AnyContent] = Action.async { request: Request[AnyContent] =>
     request
       .body
       .asJson
       .map(_.validate[List[Int]])
       .flatMap(_.asOpt)
-      .map(x=>solve(x))
-      .map(x=>Ok(Json.toJson(x)))
-      .getOrElse(BadRequest(Json.toJson("")))
-  }
+      .map(x=>solveAndSave(x).map((s)=>Ok(Json.toJson(s))))
+      .getOrElse(Future{BadRequest("")})
+}
 
-  def get(key: UUID): Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
+  def get(key: String): Action[AnyContent] = Action.async {
     readSolution(key)
-      .map(x=>Ok(Json.toJson(x)))
-      .getOrElse(NotFound(""))
+      .map(_
+        .map(value=>Ok(Json.toJson(value)))
+        .getOrElse(NotFound(""))
+      )
   }
 }
--- app/service/Service.scala
+++ app/service/Service.scala
@@ -1,17 +1,15 @@
 package service
 
-import java.util.UUID
+import database.SolutionRepository
+import solver.Solver.solve
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 object Service {
-  def solve(problem:Seq[Int]):UUID={
-    //TODO
-    UUID.randomUUID()
-  }
-  def saveSolution(key:UUID,solution:Seq[Int]):Unit={
-//TODO
-  }
-  def readSolution(key:UUID):Option[Seq[Int]]={
-    //TODO
-    None
-  }
+  def solveAndSave(problem: Seq[Int]): Future[String] =
+    SolutionRepository.create(solve(problem)).map(_.toString)
+
+  def readSolution(key: String)(implicit context :ExecutionContext): Future[Option[Array[Int]]] =
+    SolutionRepository.find(key)(context)
 }
