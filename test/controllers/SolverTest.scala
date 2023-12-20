f462b1b0114f338a3c79abc2ac693f81e3cc06c6
hannah <hannahellis4242@gmail.com>
2023-12-20 12:47:58 +0000

    adding tests

--- test/controllers/SolverTest.scala
+++ test/controllers/SolverTest.scala
@@ -103,8 +103,22 @@ class SolverTest extends AnyFlatSpec with Matchers {
       (p: Params) => Solver.sequence(p.input) should be(p.expected)))
   }
 
+  private def testSeparated(): Unit = {
+    case class Params(input: Seq[Int], expected: Seq[Seq[Int]])
+    val params = List(
+      Params(List[Int](), Vector[Vector[Int]]()),
+      Params(List(1), Vector(Vector(4, 1, 2, 3))),
+      Params(List(2), Vector(Vector(8, 1, 2, 7, 6, 3, 4, 5))),
+      Params(List(1, 1), Vector(Vector(4, 1, 2, 3),Vector( 8, 5, 6, 7))),
+      Params(List(2, 1), Vector(Vector(8, 1, 2, 7, 6, 3, 4, 5),Vector( 12, 9, 10, 11))))
+    params.foreach(params => runTest[Params]("sequence test",
+      params,
+      (p: Params) => Solver.separated(p.input) should be(p.expected)))
+  }
+
   testPageNumbers()
   testSequence()
+  testSeparated()
 
   println(Console.GREEN + "\n\n******************")
   println(Console.GREEN + "*** ALL PASSED ***")
