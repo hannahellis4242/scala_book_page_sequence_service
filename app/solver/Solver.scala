8d1d76a9aebb423f4cf339642392eb68e3973787
hannah <hannahellis4242@gmail.com>
2023-12-21 10:02:12 +0000

    forgot the code for tests too

--- app/solver/Solver.scala
+++ app/solver/Solver.scala
@@ -1,20 +1,9 @@
 package solver
 
+import solver.PageNumber.pageNumber
 import utils.Utils.swap
 
 object Solver {
-  def pageNumber(size: Int, index: Int): Option[Int] =
-    if (index >= 4 * size)
-      None
-    else {
-      val sheet = index / 4
-      index % 4 match {
-        case 0 => Some(4 * size - 2 * sheet)
-        case 1 | 2 => Some(index - 2 * sheet)
-        case 3 => Some(4 * size - 2 * sheet - 1)
-      }
-    }
-
   private def calculatePages(x: Seq[Int]): Seq[Seq[Int]] =
     swap(x
       .map(size => swap(Range(0, 4 * size)
