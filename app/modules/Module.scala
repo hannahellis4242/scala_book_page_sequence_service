0b10b1e91424b3e5613e0fbfa270c1918710351f
Hannah Ellis <hannahellis4242@gmail.com>
2023-12-27 11:04:39 +0000

    Use query params instead (#9)
    * making code work by query param instead
    
    * Adding environment variable to set the database host name

--- app/modules/Module.scala
+++ app/modules/Module.scala
@@ -7,7 +7,7 @@ import play.api.{Configuration, Environment}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class Module extends play.api.inject.Module {
-  override def bindings(environment: Environment, configuration: Configuration): collection.Seq[Binding[_]] = {
-    Seq(bind[SolutionRepository].toInstance(new SolutionRepositoryRedis("redis")))
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+    Seq(bind[SolutionRepository].toInstance(new SolutionRepositoryRedis(sys.env.getOrElse("DB-HOST", "localhost"))))
   }
 }
