dda01f9e61fe3b8f4b736e972c14fc81a013f01d
hannah <hannahellis4242@gmail.com>
2024-01-04 14:17:28 +0000

    various refactors to tidy up a little bit. Including renaming variables to make it a little more obvious what they are and adding some comments to the solver code

--- app/solver/PageNumber.scala
+++ app/solver/PageNumber.scala
@@ -1,15 +1,18 @@
 package solver
 
 object PageNumber {
-  def pageNumber(size: Int, index: Int): Option[Int] =
-    if (index >= 4 * size)
+  /* gives back the page number that should be printed on *
+   * the given page index (zero indexed) for a signature  *
+   * of the given size.                                   */
+  def pageNumber(sheetsInSignature: Int, pageIndex: Int): Option[Int] =
+    if (pageIndex >= 4 * sheetsInSignature)
       None
     else {
-      val sheet = index / 4
-      index % 4 match {
-        case 0 => Some(4 * size - 2 * sheet)
-        case 1 | 2 => Some(index - 2 * sheet)
-        case 3 => Some(4 * size - 2 * sheet - 1)
+      val sheet = pageIndex / 4
+      pageIndex % 4 match {
+        case 0 => Some(4 * sheetsInSignature - 2 * sheet)
+        case 1 | 2 => Some(pageIndex - 2 * sheet)
+        case 3 => Some(4 * sheetsInSignature - 2 * sheet - 1)
       }
     }
 }
--- app/solver/Solver.scala
+++ app/solver/Solver.scala
@@ -5,18 +5,19 @@ import solver.PageNumber.pageNumber
 import utils.Utils.swap
 
 object Solver {
-  private def calculatePages(x: Seq[Int]): Seq[Seq[Int]] =
-    swap(x
-      .map(size => swap(Range(0, 4 * size)
-        .map(index => pageNumber(size, index)))))
+  private def calculatePages(signatureSizes: Seq[Int]): Seq[Seq[Int]] =
+    swap(signatureSizes
+      .map(sheetsInSignature =>
+        swap(Range(0, 4 * sheetsInSignature)
+          .map(pageIndex => pageNumber(sheetsInSignature, pageIndex)))))
       .getOrElse(Vector())
 
   private def sequence(x: Seq[Int]): Seq[Int] = separated(x).flatten
 
   private def separated(x: Seq[Int]): Seq[Seq[Int]] =
     calculatePages(x)
-      .foldLeft[(Int, Seq[Seq[Int]])]((0, Vector()))((acc, x) =>
-        (acc._1 + x.length, acc._2 :+ x.map(_ + acc._1))
+      .foldLeft[(Int, Seq[Seq[Int]])]((0, Vector()))((acc, signaturePageNumbers) =>
+        (acc._1 + signaturePageNumbers.length, acc._2 :+ signaturePageNumbers.map(_ + acc._1))
       )._2
 
   def solve(problem: Problem): Solution = problem match {
